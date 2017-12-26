d065b8766d0f6117e2554630c141c172bb06311e
Fajar Yudha Pratama <fajaryudha23@gmail.com>
2017-12-26 11:23:21 +0700

    Update Widget Service

--- app/src/main/java/com/example/android/mygarden/PlantWateringService.java
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -2,9 +2,12 @@ package com.example.android.mygarden;
 
 import android.app.IntentService;
 import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
@@ -27,9 +30,12 @@ public class PlantWateringService extends IntentService {
      *
      //* @param name Used to name the worker thread, important only for debugging.
      */
+    public static final String ACTION_UPDATE_PLANT_WIDGETS = "com.example.android.mygarden.action.update_plant_widgets";
+
     public PlantWateringService() {
         super("PlantWaterngService");
     }
+
 
     public static void startActionWaterPlants(Context context){
         Intent intent = new Intent(context, PlantWateringService.class);
@@ -37,6 +43,10 @@ public class PlantWateringService extends IntentService {
         context.startService(intent);
     }
 
+    public static void startActionUpdatePlantWidgets(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_UPDATE_PLANT_WIDGETS);
+        context.startService(intent);}
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
@@ -44,7 +54,10 @@ public class PlantWateringService extends IntentService {
             final String action = intent.getAction();
             if (ACTION_WATER_PLANTS.equals(action)){
                 handleActionWatelPlants();
+            } else if (ACTION_UPDATE_PLANT_WIDGETS.equals(action)){
+                handleActionWatelPlants();
             }
+
         }
     }
 
@@ -54,7 +67,37 @@ public class PlantWateringService extends IntentService {
         long timeNow = System.currentTimeMillis();
         contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
 
-        getContentResolver().update(PLANT_URI,contentValues,PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME+">?",
+        getContentResolver().update(PLANT_URI,contentValues,PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME + ">?",
                 new String[]{String.valueOf(timeNow - PlantUtils.MAX_AGE_WITHOUT_WATER)});
     }
+
+    private void handleActionUpdatePlantWidgets() {
+                //Query to get the plant that's most in need for water (last watered)
+                        Uri PLANT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+                Cursor cursor = getContentResolver().query(
+                                PLANT_URI,
+                                null,
+                                null,
+                                null,
+                                PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME
+                                );
+                // Extract the plant details
+                int imgRes = R.drawable.grass; // Default image in case our garden is empty
+                if (cursor != null && cursor.getCount() > 0) {
+                        cursor.moveToFirst();
+                        int createTimeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_CREATION_TIME);
+                        int waterTimeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
+                       int plantTypeIndex = cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_PLANT_TYPE);
+                        long timeNow = System.currentTimeMillis();
+                        long wateredAt = cursor.getLong(waterTimeIndex);
+                        long createdAt = cursor.getLong(createTimeIndex);
+                        int plantType = cursor.getInt(plantTypeIndex);
+                        cursor.close();
+                        imgRes = PlantUtils.getPlantImageRes(this, timeNow - createdAt, timeNow - wateredAt, plantType);
+                    }
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+                int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, PlantWidgetProvider.class));
+                //Now update all widgets
+                        PlantWidgetProvider.updatePlantWidgets(this, appWidgetManager, imgRes, appWidgetIds);
+            }
 }
--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -16,15 +16,19 @@ import com.example.android.mygarden.ui.MainActivity;
 
 public class PlantWidgetProvider extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int imgRes,   int appWidgetId) {
         // Create an Intent to launch MainActivity when clicked
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
+        // Update image
+        views.setImageViewResource(R.id.widget_plant_image, imgRes);
         // Widgets allow click handlers to only launch pending intents
         views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
         // Instruct the widget manager to update the widget
+
+
 
         Intent wateringIntent = new Intent(context, PlantWateringService.class);
         wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
@@ -36,9 +40,8 @@ public class PlantWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-    // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-        updateAppWidget(context, appWidgetManager, appWidgetId);}
+    // There may be multiple widgets active, so update all of the
+        PlantWateringService.startActionUpdatePlantWidgets(context);
     }
 
     @Override
@@ -54,5 +57,11 @@ public class PlantWidgetProvider extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
     // Perform any action when the last AppWidget instance for this provider is deleted
+    }
+
+    public static void updatePlantWidgets(Context context, AppWidgetManager appWidgetManager, int imgRes, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, imgRes, appWidgetId);
+        }
     }
 }
